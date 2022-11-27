19b96dba5f1dad41fdce634bdb8f3f3039bf3494
mustahsin2000 <mustahsin152415@gmail.com>
2022-11-27 16:34:44 +0600

    category

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const cors = require('cors');
+
+require('dotenv').config()
+
+//resaleMobile
+//guSuoD1wROAhp1vN
 
 const port = process.env.PORT || 5000;
 
@@ -16,4 +23,84 @@ app.get('/',async (req,res)=>{
 //resaleMobile
 //
 
-app.listen(port,()=>console.log(`doctors portal running ${port}`))
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wfblndv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+     const categoriesCollection = client.db('resaleMobile').collection('categories');
+     const productsCollection = client.db('resaleMobile').collection('products');
+
+     app.get('/categories',async(req,res)=>{
+        const query={};
+        const category = await categoriesCollection.find(query).toArray();
+        res.send(category);
+     })
+
+     app.get('/categories/:id',async(req,res)=>{
+      const id = req.params.id;
+      const cursor = productsCollection.filter(category=>category.category === category);
+      const products = await cursor.toArray();
+      res.send(products);
+     })
+
+
+     app.get('/products',async(req,res)=>{
+        const query={};
+        const category = await productsCollection.find(query).toArray();
+        res.send(category);
+     })
+
+     app.get('/products/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {id:parseInt(id)};
+      const cursor = productsCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product);
+     })
+
+   //   app.get('/categories/:id',async(req,res)=>{
+   //      const id = req.params.id;
+   //      const query = {id:parseInt(id)}
+   //      const cursor = productsCollection.find(query);
+   //      const product = await cursor.toArray();
+   //      res.send(product);
+   //   })
+
+   // //  samsung products
+   //   app.get('/samsung',async(req,res)=>{
+   //      const filter ={category:"samsung"}
+   //      const filtering = await productsCollection.find(filter)
+   //      const filtering3 = await filtering.toArray(filtering)
+   //      res.send(filtering3)
+   //   })
+
+   //   //iphone 
+   //   app.get('/iPhone',async(req,res)=>{
+   //      const filter ={category:'iPhone'}
+   //      const filtering = await productsCollection.find(filter)
+   //      const filtering3 = await filtering.toArray(filtering)
+   //      res.send(filtering3)
+   //   })
+
+   //   //  onePlus products getting 
+   //   app.get('/oneplus',async(req,res)=>{
+   //      const filter ={category:'oneplus'}
+   //      const filtering = await productsCollection.find(filter)
+   //      const filtering3 = await filtering.toArray(filtering)
+   //      res.send(filtering3)
+   //   })
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log)
+
+
+app.listen(port,()=>console.log(`Mobile resale programming ${port}`))
